3eac4f533ff8d53291029a21ce1229fed6ec7a17
Mehmet Eroğul <mehmet_erogul@icloud.com>
2021-08-22 13:14:02 +0300

    .
    .

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    public void NewColorSelected(Color color)
+    {
+        MainManager.Instance.TeamColor = color;
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.teamColor;
+        }
+    }
+}
+
+
